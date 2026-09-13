f8a9172cfddc7988655c70b2e584cc8600c2b289
Utkarsh Punkar <utkarshpunkar7@gmail.com>
2026-09-13 23:38:04 +0530

    Fix formatting of cors middleware usage

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import routes from './routes';
 
 const app = express();
 
-app.use(cors({
+app.use(cors({ 
   origin: '*', 
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
   credentials: true,
